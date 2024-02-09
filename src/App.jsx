6c3b2e657d973be4550d6f197a88b3088cbd6e50
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-02-08 21:04:43 -0300

    estrutura inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
