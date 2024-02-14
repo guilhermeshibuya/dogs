21dd817ec330270dff0eda40cf282af1384ff9a1
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-02-14 16:31:03 -0300

    add: feed puxando fotos da api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import Feed from '../Feed/Feed';
 
 const Home = () => {
   return (
-    <section className="container">
+    <section className="container mainContainer">
       <Feed />
     </section>
   );
