7b7fef9e63bea17e58f56d5905ab446a90b6e154
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-02-15 15:48:11 -0300

    add: feed modal

--- src/components/Feed/FeedModal.jsx
+++ src/components/Feed/FeedModal.jsx
@@ -1,5 +1,30 @@
-const FeedModal = () => {
-  return <div>FeedModal</div>;
+import styles from './FeedModal.module.css';
+import useFetch from '../../hooks/useFetch';
+import { useEffect } from 'react';
+import { PHOTO_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent';
+
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
---
+++ src/components/Photo/PhotoComments.jsx
@@ -0,0 +1,5 @@
+const PhotoComments = () => {
+  return <div>PhotoComments</div>;
+};
+
+export default PhotoComments;
