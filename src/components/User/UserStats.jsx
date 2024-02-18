ef73fe103f0d873c5331d09e6fba0b94ab8a4995
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2024-02-18 16:04:54 -0300

    add: graficos nas estatisticas do usuario, lazy e suspense para carregar a biblioteca de grafico

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+
+const UserStatsGraphs = lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      {' '}
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
